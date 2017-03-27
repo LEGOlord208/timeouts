478595bace8de30516b032fe33648f8e5ecc86b0
LEGOlord208 <LEGOlord208@gmail.com>
2017-03-27 21:21:11 +0200

    ListTimeouts and PruneTimeouts

--- timeouts.go
+++ timeouts.go
@@ -22,15 +22,51 @@ func (to *Timeout) SetTimeout(handle string, duration time.Duration){
 	to.timeouts[handle] = time.Now().UTC().Add(duration);
 }
 
-// Checks if 'handle' is (still) in a timeout
+// Checks if 'handle' is (still) in a timeout.
+// Also removes the handle from the timeout to save resources.
 func (to *Timeout) InTimeout(handle string) bool{
 	now := time.Now().UTC();
 
 	timeout, ok := to.timeouts[handle];
-	return ok && (now.Equal(timeout) || now.Before(timeout));
+	val := ok && (now.Before(timeout) || now.Equal(timeout));
+
+	if(val){
+		to.RemoveTimeout(handle);
+	}
+	return val;
 }
 
 // Forcefully remove 'handle' from a timeout, if any.
 func (to *Timeout) RemoveTimeout(handle string){
 	delete(to.timeouts, handle);
+}
+
+// List handles in timeout.
+// Also removes unnecessary handles.
+// No order guaranteed.
+func (to *Timeout) ListTimeouts() []string{
+	now := time.Now().UTC();
+
+	var handles []string;
+	for handle, timeout := range to.timeouts{
+		if(now.Before(timeout) || now.Equal(timeout)){
+			handles = append(handles, handle);
+		}
+	}
+	return handles;
+}
+
+// Prune passed timeouts.
+// Return removed handles.
+func (to *Timeout) PruneTimeouts() []string{
+	now := time.Now().UTC();
+
+	var handles []string;
+	for handle, timeout := range to.timeouts{
+		if(now.After(timeout)){
+			handles = append(handles, handle);
+			to.RemoveTimeout(handle);
+		}
+	}
+	return handles;
 }
